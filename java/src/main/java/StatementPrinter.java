5c497ad664633483dc891d0ec2a7090184d1da65
Gregor Riegler <gregor.riegler@ebcont.com>
2019-09-27 20:25:36 +0200

    !!!: slide statement

--- java/src/main/java/StatementPrinter.java
+++ java/src/main/java/StatementPrinter.java
@@ -13,11 +13,11 @@ public class StatementPrinter {
 
         for (var perf : invoice.performances) {
             volumeCredits += getVolumeCredits(plays, perf);
+            totalAmount += calcAmount(perf, plays.get(perf.playID));
 
             // print line for this order
             LineData lineData = new LineData(plays.get(perf.playID).name, calcAmount(perf, plays.get(perf.playID)) / 100, perf.audience);
             result.append(String.format("  %s: %s (%s seats)\n", lineData.getPlayName(), frmt.format(lineData.getAmount()), lineData.getAudience()));
-            totalAmount += calcAmount(perf, plays.get(perf.playID));
         }
         result.append(String.format("Amount owed is %s\n", frmt.format(totalAmount / 100)));
         result.append(String.format("You earned %s credits\n", volumeCredits));
