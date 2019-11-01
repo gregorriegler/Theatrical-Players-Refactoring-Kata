7f96c08eacdca614181accb159f22ae1aa3e846d
Gregor Riegler <gregor.riegler@ebcont.com>
2019-11-01 20:44:07 +0100

    splitted

--- javascript/src/statement.js
+++ javascript/src/statement.js
@@ -1,14 +1,5 @@
-
-function statement (invoice, plays) {
-    let totalAmount = 0;
-    let volumeCredits = 0;
-    let result = `Statement for ${invoice.customer}\n`;
-    const format = new Intl.NumberFormat("en-US",
-        { style: "currency", currency: "USD",
-            minimumFractionDigits: 2 }).format;
-
-    for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
+function calculateInvoiceData(invoice, plays) {
+    function calcAmount(play, perf) {
         let thisAmount = 0;
         switch (play.type) {
             case "tragedy":
@@ -25,17 +16,80 @@ function statement (invoice, plays) {
                 thisAmount += 300 * perf.audience;
                 break;
         }
-        // add volume credits
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-        // print line for this order
-        result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
-        totalAmount += thisAmount;
+        return thisAmount;
     }
-    result += `Amount owed is ${format(totalAmount/100)}\n`;
-    result += `You earned ${volumeCredits} credits\n`;
+
+    function calcLineData(invoice, plays) {
+        let lineDatas = [];
+        for (let perf of invoice.performances) {
+            lineDatas.push({
+                name: plays[perf.playID].name,
+                totalAmount: calcAmount(plays[perf.playID], perf),
+                audience: perf.audience
+            });
+        }
+        return lineDatas;
+    }
+
+    function calcTotalVolumeCredits(invoice, plays) {
+        let volumeCredits = 0;
+        for (let perf of invoice.performances) {
+            volumeCredits += calcVolumeCredits(perf, plays);
+        }
+        return volumeCredits;
+    }
+
+    function calcVolumeCredits(perf, plays) {
+        let thisVolumeCredits = Math.max(perf.audience - 30, 0);
+        if ("comedy" === plays[perf.playID].type) thisVolumeCredits += Math.floor(perf.audience / 5);
+        return thisVolumeCredits;
+    }
+
+    function calcTotalAmount(invoice, plays) {
+        let totalAmount = 0;
+        for (let perf of invoice.performances) {
+            totalAmount += calcAmount(plays[perf.playID], perf);
+        }
+        return totalAmount;
+    }
+
+    return {
+        customer: invoice.customer,
+        lines: calcLineData(invoice, plays),
+        totalAmount: calcTotalAmount(invoice, plays),
+        volumeCredits: calcTotalVolumeCredits(invoice, plays)
+    };
+}
+
+function statement(invoice, plays) {
+    const invoiceData = calculateInvoiceData(invoice, plays);
+    return print(invoiceData, plainTextPrinter);
+}
+
+function plainTextPrinter(invoiceData) {
+    let result = `Statement for ${invoiceData.customer}\n`;
+    for (const lineData of invoiceData.lines) {
+        result += printLine(lineData, usFormat());
+    }
+    result += `Amount owed is ${usFormat()(invoiceData.totalAmount / 100)}\n`;
+    result += `You earned ${invoiceData.volumeCredits} credits\n`;
     return result;
+}
+
+function print(invoiceData, printer) {
+    return printer(invoiceData);
+}
+
+function printLine(lineData, format) {
+    return ` ${lineData.name}: ${format(lineData.totalAmount / 100)} (${lineData.audience} seats)\n`;
+}
+
+function usFormat() {
+    return new Intl.NumberFormat("en-US",
+        {
+            style: "currency", currency: "USD",
+            minimumFractionDigits: 2
+        }).format;
 }
 
 module.exports = statement;
